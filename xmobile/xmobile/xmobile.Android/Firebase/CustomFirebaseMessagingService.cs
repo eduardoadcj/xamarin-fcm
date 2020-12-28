ad8fbca897a9bf2b970249c5be786a4a27335d4f
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-12-28 17:16:18 -0300

    Incluindo serviço de mensagem

---
+++ xmobile/xmobile/xmobile.Android/Firebase/CustomFirebaseMessagingService.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Firebase.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XamarinFirebaseIntegration.Droid.Firebase
+{
+    [Service(Exported = false)]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class CustomFirebaseMessagingService : FirebaseMessagingService 
+    {
+
+        private const string TAG = "CustomFirebaseMessagingService";
+
+        public override void OnMessageReceived(RemoteMessage remoteMessage)
+        {
+            base.OnMessageReceived(remoteMessage);
+            Log.Debug(TAG, "OnMessageReceived!");
+        }
+
+    }
+}
