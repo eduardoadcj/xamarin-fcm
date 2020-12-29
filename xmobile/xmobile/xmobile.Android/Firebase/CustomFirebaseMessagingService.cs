2ce766bd89b160142cc2c4a6e265a8491aea2909
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-12-29 11:18:04 -0300

    Incluindo tratativa de mensagem

--- xmobile/xmobile/xmobile.Android/Firebase/CustomFirebaseMessagingService.cs
+++ xmobile/xmobile/xmobile.Android/Firebase/CustomFirebaseMessagingService.cs
@@ -1,10 +1,12 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using AndroidX.Core.App;
 using Firebase.Messaging;
 using System;
 using System.Collections.Generic;
@@ -18,12 +20,43 @@ namespace XamarinFirebaseIntegration.Droid.Firebase
     public class CustomFirebaseMessagingService : FirebaseMessagingService 
     {
 
+        private const string NOTIFICATION_CHANNEL_ID = "23432";
         private const string TAG = "CustomFirebaseMessagingService";
 
         public override void OnMessageReceived(RemoteMessage remoteMessage)
         {
             base.OnMessageReceived(remoteMessage);
             Log.Debug(TAG, "OnMessageReceived!");
+
+            if (remoteMessage.Data.ContainsKey("Type") 
+                && remoteMessage.Data["Type"] == "Sync_Request")
+            {
+                DispatchNoticeNotification("Sync", "Sync request");
+            }
+            else
+            {
+                var notification = remoteMessage.GetNotification();
+                DispatchNoticeNotification(notification.Title, notification.Body);
+            }
+        }
+
+        private void DispatchNoticeNotification(string title, string content)
+        {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,
+                "canal", NotificationImportance.Max);
+            notificationChannel.EnableLights(true);
+
+            NotificationManager notificationManager = (NotificationManager)GetSystemService(Context.NotificationService);
+            notificationManager.CreateNotificationChannel(notificationChannel);
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+                .SetSmallIcon(Resource.Drawable.IcPopupReminder)
+                .SetPriority(NotificationCompat.PriorityDefault)
+                .SetContentTitle(title)
+                .SetContentText(content);
+
+            NotificationManagerCompat.From(this)
+                .Notify(1000, builder.Build());
         }
 
     }
